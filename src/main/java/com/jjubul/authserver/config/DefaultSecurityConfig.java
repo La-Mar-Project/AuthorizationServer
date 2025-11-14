1c94fea482e77d7bf63fb89c28e3c21d310a49fe
Jongyun Won <jywon1128@gmail.com>
2025-11-14 18:48:29 +0900

    for deploy

--- src/main/java/com/jjubul/authserver/config/DefaultSecurityConfig.java
+++ src/main/java/com/jjubul/authserver/config/DefaultSecurityConfig.java
@@ -2,14 +2,20 @@ package com.jjubul.authserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.server.authorization.web.OAuth2AuthorizationEndpointFilter;
+import org.springframework.security.oauth2.server.authorization.web.OAuth2TokenEndpointFilter;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
+import org.springframework.security.web.context.SecurityContextHolderFilter;
 
 @Configuration
 public class DefaultSecurityConfig {
@@ -17,10 +23,13 @@ public class DefaultSecurityConfig {
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests(request -> request
-                .requestMatchers("/", "/auth/**", "/oauth2/**", "/.well-known/jwks.json", "/errors").permitAll()
-                .anyRequest().authenticated())
-                .csrf(AbstractHttpConfigurer::disable);
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(request -> request
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .requestMatchers("/", "/auth/**", "/oauth2/**", "/.well-known/jwks.json", "/error").permitAll()
+                .anyRequest().authenticated());
 
         return http.build();
     }
